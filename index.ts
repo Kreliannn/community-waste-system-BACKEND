0acb0026d8096f06128f945d1280707cc486a048
Kreliannn <krelianquimson@gmail.com>
2025-04-27 17:46:46 +0800

    integrate socket io

--- index.ts
+++ index.ts
@@ -1,12 +1,40 @@
-import express,{ Request, Response } from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-const app = express();
-const port = 3000;
+dotenv.config();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello world');
+const port = process.env.PORT
+const db = process.env.DB as string
+
+mongoose.connect(db)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+const httpServer = http.createServer(); 
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+io.on('connection', (socket: Socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on("login", (username : string, password : string) => {
+    (username === "krel" && password === "123") ? socket.emit("loginSuccess") : socket.emit("loginFailed")
+  })
+
+
 });
+
+httpServer.listen(port, () => {
+  console.log('Socket.IO server running at http://localhost:' + port);
+});
+
